faa37abac645ff699aaddddd4004ba4406a8c9f2
hasei-si <magunav5@gmail.com>
2025-12-08 20:00:18 +0900

    Playerの体力UIを作った

---
+++ Assets/Script/HPBar.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : MonoBehaviour
+{
+    public Image fillImage; // HPバーの中身（Image）
+
+    public void SetHP(float ratio)
+    {
+        // ratio は 0〜1 で渡す（0 = 空 / 1 = 満タン）
+        fillImage.fillAmount = ratio;
+    }
+}
